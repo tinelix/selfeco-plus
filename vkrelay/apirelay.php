ce7fb13d224257627fec380febd9f92d79ce53ca
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2026-04-14 18:30:58 +0700

    Update apirelay.php

--- vkrelay/apirelay.php
+++ vkrelay/apirelay.php
@@ -2,15 +2,22 @@
 	ob_start();
 	error_reporting(E_ALL);
 
-
 	header("Content-Type: text/json");
-	if(isset($_GET["goto"]))
+	if(strlen($_GET["goto"]) > 0)
 		$request = $_GET["goto"];
 	else
 		$request = file_get_contents("php://input");
 	
 	$curl = curl_init($request);
 	curl_exec($curl);
+
+	$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+	if($statusCode === 301 && str_starts_with($request, "http://")) {
+		$request = str_replace("http://", "https://", $request);
+		$curl = curl_init($request);
+		curl_exec($curl);
+	}
 	
 	header("Content-length: " . ob_get_length());
 	ob_flush();
